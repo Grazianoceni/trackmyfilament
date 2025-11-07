81bc19a7d4c60b5afaf7f0c643fc1e15025251e9
Grazianoceni <55365463+Grazianoceni@users.noreply.github.com>
2025-11-07 19:21:15 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,24 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('filamenti-cache').then(cache => {
-      return cache.addAll(['index.html', 'manifest.json', 'icon-94.png']);
+const CACHE_NAME = "filament-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
